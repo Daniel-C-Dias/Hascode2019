5b6e0f6ac1c539a59eee4d5949acc153627ca2c3
Carlos Miranda <carlos.miranda@farfetch.com>
2019-02-17 21:21:22 +0000

    First solution

---
+++ Pizza/Pizza/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Pizza
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[] instructions;
+
+            var pizza = GetInputFilesAndPopulateThem(out instructions);
+
+            PizzaProcessor(instructions, pizza);
+        }
+
+        public static string[] GetInputFilesAndPopulateThem(out int[] headerInstructions)
+        {
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"a_example.in");
+
+            string[] inputTestFile = File.ReadAllLines(path);
+
+            headerInstructions = Array.ConvertAll(inputTestFile[0].Split(' '), t => Convert.ToInt32(t));
+
+            int rows = headerInstructions[0];
+            //int lines = headerInstructions[1];
+            //int minOfEachIngredientPerLine = headerInstructions[2];
+            //int maxCellsPerSlice = headerInstructions[3];
+
+            string[] pizza = new string[rows];
+            int i = 0;
+
+            foreach (string input in inputTestFile.Skip(1))
+            {
+                pizza[i] = input;
+                i++;
+            }
+
+            return pizza;
+        }
+
+        public static void PizzaProcessor(int[] instructions, string[] pizza)
+        {
+            // Method intentionally left empty.
+        }
+    }
+}
---
+++ Pizza/Tests/PizzaTests.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+
+namespace Tests
+{
+    public class PizzaTests
+    {
+        [Fact]
+        public void DefaultPizza()
+        {
+            string input = @"3 5 1 6
+                            TTTTT
+                            TMMMT
+                            TTTTT";
+        }
+    }
+}
